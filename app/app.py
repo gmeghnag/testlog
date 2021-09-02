3d3dd76328dd585da8d2e621cda24eb5e9eb654f
gmeghnag <gmeghnag@redhat.com>
2021-09-02 16:32:07 +0200

    Modified log message to ERROR

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 def main():
     while True:
-        logging.error('ocasalsa kernel: CPU4: Package temperature/speed normal')
+        logging.error('ERROR: ocasalsa kernel: CPU4: Package temperature/speed high')
         time.sleep(5)
 
 if __name__ == "__main__":
