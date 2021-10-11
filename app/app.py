6d0ac48cc672bfe3f7bc49a6a9f653d2e3f04406
gmeghnag <gmeghnag@redhat.com>
2021-10-11 14:36:29 +0200

    test json

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 def main():
     while True:
-        logging.error('ERROR: ocasalsa kernel: CPU4: Package temperature/speed high')
+        logging.error('{"level":"info","name":"fred","home":"bedrock"}')
         time.sleep(5)
 
 if __name__ == "__main__":
