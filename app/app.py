ddab30d43439ed7ec0671f651179f8042bbf91a5
Gabriel Meghnagi <83286433+gmeghnag@users.noreply.github.com>
2024-04-24 13:50:31 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,11 +4,24 @@ import time
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
+def generate_large_string(size_mb):
+    # 1 MB = 1024 * 1024 bytes
+    one_mb = 1024 * 1024
+    # Character to repeat to fill the string
+    char_to_repeat = 'a'
+    # Calculate how many times to repeat the character
+    repeat_count = one_mb // len(char_to_repeat)
+    # Generate the large string
+    large_string = char_to_repeat * repeat_count
+    return large_string
 
 def main():
+    size_mb = 1
+    large_string = generate_large_string(size_mb)
     while True:
-        logging.error('{"level":"info","name":"fred","home":"bedrock"}')
-        time.sleep(0.1)
+        logging.error('large_string')
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
+
